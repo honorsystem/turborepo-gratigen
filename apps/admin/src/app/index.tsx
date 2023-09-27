0061ea6904a2f10ac275c9679033f278e2db19bc
honorsystem <142290396+honorsystem@users.noreply.github.com>
2023-09-26 22:53:22 -0600

    Update index.tsx

--- apps/admin/src/app/index.tsx
+++ apps/admin/src/app/index.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
     <div className="container">
       <h1 className="title">
         Admin <br />
-        <span>Kitchen Sink</span>
+        <span>Kitchen bink</span>
       </h1>
       <CounterButton />
       <p className="description">
